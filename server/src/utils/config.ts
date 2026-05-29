fc3ff8ac358b7163186e7c6c5b89cc9216900afe
Elhanan <alhanan@gmail.com>
2026-05-29 14:57:17 +0300

    add default values for config

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -2,6 +2,6 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export const config = {
-    port: process.env.PORT,
-    mongo_uri: process.env.MONGO_URI
+    port: process.env.PORT || 3000,
+    mongo_uri: process.env.MONGO_URI || 'mongodb://localhost:27017/flight-monitor'
 }
