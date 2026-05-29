ae3c161cf6a0c3e5040e3803e09395b1f2a8fe14
Elhanan <alhanan@gmail.com>
2026-05-29 16:41:29 +0300

    update configuration of db and add validation utility

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -3,11 +3,23 @@ import { config } from '@/utils/config';
 import { logger } from '@/utils/logger';
 
 export const connectDB = async () => {
-    try {
-        const mongooseConnection = await mongoose.connect(config.mongo_uri);
-        logger.info(`MongoDB connected successfully by host: ${mongooseConnection.connection.host}`);
+    try { 
+        const db = mongoose.connection;
+        db.once("open", () => logger.info(`connect to mongoDB host: ${db.host}`));
+        db.on('error', (err) => logger.error(`Error in db: ${err}`));
+        
+        await mongoose.connect(config.mongo_uri);
     } catch (error) {
         logger.error('MongoDB connection error:', error);
         process.exit(1);
+    }
+}
+
+export const closeConnectionDB = async () => {
+    try {
+        await mongoose.connection.close();
+        logger.info("MongoDB connection closed")
+    } catch (error) {
+        logger.error(`MongoDB closing error: ${error}`);
     }
 }
--- server/src/controller/flight-monitor.controller.ts
+++ server/src/controller/flight-monitor.controller.ts
@@ -3,9 +3,33 @@ import { Response, Request } from 'express';
 import { logger } from '@/utils/logger';
 import { FlightDataType } from '@/types/types';
 
+interface FlightDataResponse {
+    success: boolean;
+    message?: string;
+}
+
+const checkFlightData = (altitude: number, hsi: number, adi: number): FlightDataResponse => {
+    if (altitude > 3000 || altitude < 0)
+        return { success: false, message: 'Altitude must be between 0 and 3000' };
+
+    if (hsi > 360 || hsi < 0)
+        return { success: false, message: 'HSI must be between 0 and 360' };
+
+    if (adi > 100 || adi < -100)
+        return { success: false, message: 'ADI must be between -100 and 100' };
+
+    return { success: true };
+}
+
 export const setFlightData = async (req: Request, res: Response) => {
     try {
         const { altitude, hsi, adi } = req.body;
+        const check: FlightDataResponse = checkFlightData(altitude, hsi, adi);
+        if (!check?.success){
+            logger.warn(`Invalid flight data: ${check.message}`);
+             return res.status(400).json(check);
+        } 
+
         await FlightData.deleteMany({});
 
         const flightData: FlightDataType | null = await FlightData.create({ altitude, hsi, adi });
